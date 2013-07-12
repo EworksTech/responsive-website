35319d7c5e6cc504c3ef1ebe665e34bcba37d8af
Cesar Schneider <cesschneider@gmail.com>
2013-07-12 12:44:55 -0300

    Fixed typo on english version

--- eworks_lading/send.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-error_log(date('r') ."|". $_SERVER['REMOTE_ADDR'] ."|". implode('|', $_POST) ."\n", 3, 'contacts.tsv');
-mail('cesar@eworks.com.br', 'Contact from website', $_SERVER['REMOTE_ADDR'] ."\n". print_r($_POST, true));
-
-?>
